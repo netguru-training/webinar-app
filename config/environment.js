4e86ea6237431d20738c933460e7c93df1d7a232
Radosław Piątek <radoslaw.piatek@netguru.pl>
2015-05-30 10:45:16 +0200

    Move application to pods structure

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'webinar-app',
-    podModulePrefix: 'bashguru/pods',
+    podModulePrefix: 'webinar-app/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
@@ -21,11 +21,11 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
